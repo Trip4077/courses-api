32aad5d7445cf9acf89602e5f140739b47d71c14
Trip4077 <bj2336@email.vccs.edu>
2020-06-19 17:43:44 -0400

    Programs.getFullProgram(id) v1

--- models/table_models/program_model.js
+++ models/table_models/program_model.js
@@ -1,5 +1,31 @@
 const BaseModel = require( '../base_model' );
+const db = require( '../../data/config' );
 
-class Programs extends BaseModel {}
+class Programs extends BaseModel {
+    async getFullProgram(id) {
+        return await db( this.name ).select(
+                                                'Programs.title as program', 
+                                                'Programs.description as program_details', 
+                                                'Programs.price as program_price', 
+                                                'Programs.duration as program_length', 
+                                                'Programs.topic as topic', 
+                                                'Courses.title as course', 
+                                                'Courses.description as course_details', 
+                                                'Courses.price as course_price', 
+                                                'Courses.duration as course_length', 
+                                                'Certificates.URL as certificate',
+                                                'Instructors.uid as instructor'
+                                            )
+                                            .innerJoin(
+                                                        'Courses', 'Programs.id', '=', 'Courses.program_id'
+                                                      )
+                                            .innerJoin(
+                                                        'Certificates', 'Programs.certificate_id', '=', 'Certificates.id'
+                                                      )
+                                            .innerJoin(
+                                                        'Instructors', 'Courses.instructor_id', '=', 'Instructors.id'
+                                                      );
+    }
+}
 
 module.exports = new Programs( 'Programs' );
