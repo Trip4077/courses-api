e75e851fd85dbc9db1a72c814e1d6fa45be6a786
Trip4077 <bj2336@email.vccs.edu>
2020-06-20 20:19:38 -0400

    Courses.getAllCourses()

--- models/table_models/course_model.js
+++ models/table_models/course_model.js
@@ -1,18 +1,17 @@
 const BaseModel = require( '../base_model' );
+const Units = require( './unit_model' );
+const Materials = require(  './material_model');
 const db = require( '../../data/config' );
 
 class Courses extends BaseModel {
-    async getUnits( course_id ) {
-        const courses = await db( this.name ).select( 'Courses.*', 'Units.title as unit_name', 'Units.id as unit_id' )
-                                    .where({course_id})
-                                    .innerJoin( 'Units', 'Courses.id', '=', 'Units.course_id' )
+    async getAllCourses() {
+        const courses = await this.getAll();
 
         for(let i = 0; i < courses.length; i++) {
-            const materials = await db( 'Materials' ).where({ unit_id: courses[i].unit_id });
-
-            courses[i].materials = materials;
+            const units = await Units.getBy({ course_id: courses[i].id });
+            courses[i].units = units;
         }
-        
+
         return courses;
     }
 }
--- models/table_models/unit_model.js
+++ models/table_models/unit_model.js
@@ -1,5 +1,24 @@
 const BaseModel = require( '../base_model' );
 
-class Units extends BaseModel {}
+const db = require( '../../data/config' );
+
+class Units extends BaseModel {
+    async getUnitData( unit_id ) {
+        const unit_materials = await db( 'Units' ).innerJoin('Materials', 'Units.id', '=', 'Materials.unit_id')
+                                  .select('Units.*', 'Materials.name as materials_name', 'Materials.URL as materials_link')
+                                  .where('Units.id', '=', `${unit_id}`)
+
+        const unit_data = {
+            id: unit_materials[0].id,
+            title: unit_materials[0].title,
+            course_id: unit_materials[0].course_id,
+            materials: []
+        }
+
+        unit_materials.forEach(material => unit_data.materials.push({ name: material.materials_name, link: material.materials_link }));
+
+        return unit_data;
+    }
+}
 
 module.exports = new Units( 'Units' );
