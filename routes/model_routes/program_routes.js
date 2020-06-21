e8ba25ee3435dfd3e91dde036a98341f18698ac2
Trip4077 <bj2336@email.vccs.edu>
2020-06-20 23:05:14 -0400

    DELETE route

--- routes/model_routes/program_routes.js
+++ routes/model_routes/program_routes.js
@@ -7,8 +7,10 @@ router.get('/', async (_, res) => {
     try {
         const programs = await Programs.getAllPrograms();
 
-        res.status(200).json({ programs });
-    } catch(err) {
+        res.status( 200 ).json({ programs });
+    } 
+    
+    catch(err) {
         console.log(('-' * 10) + err + ('-' * 10));
 
         res.status(500).json({ message: "Error Getting Programs", err });
@@ -17,28 +19,54 @@ router.get('/', async (_, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        if( !utils.validateIdParam( req.params.id ) ) return res.status(403).json({ message: "Invalid ID", id: req.params.id });
+        if( !utils.validateIdParam( req.params.id )) res.status( 403 ).json({ message: "Invalid ID", id: req.params.id });
 
         const program = await Programs.getProgramById( req.params.id );
 
         res.status(200).json({ program });
-    } catch(err) {
+    } 
+    
+    catch(err) {
         console.log(('-' * 10) + err + ('-' * 10));
 
         res.status(500).json({ message: "Error Getting Program", err });
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async ( req, res ) => {
+    try {
+
+    } 
     
+    catch( err ) {
+        console.log(('-' * 10) + err + ('-' * 10));
+    }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async ( req, res ) => {
+    try {
 
+    } 
+    
+    catch( err ) {
+        console.log(('-' * 10) + err + ('-' * 10));
+    }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async ( req, res ) => {
+    try {
+        if( !utils.validateIdParam( req.params.id )) res.status( 403 ).json({ message: "Invalid ID", id: req.params.id });
 
+        const result = await Programs.remove( req.params.id );
+
+        res.status(200).json({ message: `Program:${req.params.id} removed`, result });
+    } 
+
+    catch( err ) {
+        console.log(('-' * 10) + err + ('-' * 10));
+
+        res.status(500).json({ message: `Error Deleting Program:${req.params.id}` });
+    }
 })
 
 module.exports = router;
