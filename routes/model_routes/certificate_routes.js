a51edce7d6c41eee225f105f5c8911f6019e1baf
Trip4077 <bj2336@email.vccs.edu>
2020-06-22 18:28:11 -0400

    PUT

--- routes/model_routes/certificate_routes.js
+++ routes/model_routes/certificate_routes.js
@@ -42,4 +42,18 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id ) || !req.body) res.status(403).json({ message: "Invalid ID or Update", id: req.params.id, body: req.body });
+
+        const updated_certificate = await Certificates.update( req.params.id, req.body );
+
+        res.status(200).json({ message: "Certificate Updated Successfully", updated_certificate });
+    } catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Updating Certficate", err});
+    }
+})
+
 module.exports = router;
