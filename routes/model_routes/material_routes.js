706e52f82a54f45897eb9fb665b66530dab03106
Trip4077 <bj2336@email.vccs.edu>
2020-06-21 20:47:12 -0400

    Materials PUT and DELETE

--- routes/model_routes/material_routes.js
+++ routes/model_routes/material_routes.js
@@ -42,4 +42,32 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id ) || !req.body) res.status(403).json({ message: "Invalid ID or Update", id: req.params.id, body: req.body });
+
+        const updated_material = await Materials.update( req.params.id, req.body );
+
+        res.status(200).json({ message: "Material Updated", updated_material });
+    } catch(err) {
+        console.log(('-' * 10) + err + ('-' * 10));
+
+        res.status(500).json({ message: "Error Updating Material", err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id )) res.status(403).json({ message: "Invalid ID", id: req.params.id });
+
+        const result = await Materials.remove( req.params.id );
+    
+        res.status(200).json({ message: `Material:${req.params.id} removed`, result });
+    } catch(err) {
+        console.log(('-' * 10) + err + ('-' * 10));
+
+        res.status(500).json({ message: "Error Removing Material", err });
+    }
+})
+
 module.exports = router;
