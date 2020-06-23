6ed41e63866fffd78a87c73d102d1297e4c05b97
Trip4077 <bj2336@email.vccs.edu>
2020-06-23 15:39:28 -0400

    POST, PUT, and DELETE

--- routes/model_routes/admin_routes.js
+++ routes/model_routes/admin_routes.js
@@ -28,4 +28,46 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const admin = req.body;
+
+        const new_admin = await Admins.insert( admin );
+
+        res.status(201).json({ message: "Admin Added Successfully", new_admin });
+    } catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Getting Admins", err });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id ) || !req.body) res.status(403).json({ message: "Invalid ID or Update", id: req.params.id, body: req.body });
+
+        const updated_admin = await Admins.update( req.params.id, req.body );
+
+        res.status(200).json({ message: "Admin Updated", updated_admin });
+    } catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Updating Admins", err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id )) res.status(403).json({ message: "Invalid ID", id: req.params.id });
+
+        const result = await Admins.remove( req.params.id );
+    
+        res.status(200).json({ message: `Admin:${req.params.id} removed`, result });
+    } catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Removing Unit", err });
+    }
+})
+
 module.exports = router;
