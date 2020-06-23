80ed5fb4ccf0e3186552495fd05aad8a5309d93e
Trip4077 <bj2336@email.vccs.edu>
2020-06-23 18:46:47 -0400

    POST and PUT

--- routes/model_routes/message_routes.js
+++ routes/model_routes/message_routes.js
@@ -32,4 +32,36 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const message = req.body;
+
+        const new_message = await Messages.insert( message );
+
+        res.status(201).json({ message: "Messages Added Succesfully", new_message });
+    }
+
+    catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Getting Message", err });    
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id )) res.status(403).json({ message: "Invalid ID", id: req.params.id });
+
+        const updated_message = await Messages.update( req.params.id, req.body);
+
+        res.status(200).json({ message: "Message Updated Successfully", updated_message });
+    }
+
+    catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Getting Message", err });   
+    }
+});
+
 module.exports = router;
