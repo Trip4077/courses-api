2e729418483d5f7946895ce249bc8955aea073d2
Trip4077 <bj2336@email.vccs.edu>
2020-06-24 12:50:18 -0400

    POST, PUT, and DELETE

--- routes/model_routes/board_routes.js
+++ routes/model_routes/board_routes.js
@@ -32,4 +32,52 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const board = req.body;
+
+        const new_board = await Boards.insert( board );
+
+        res.status(201).json({ message: "Board Added Successfully", new_board });
+    } 
+    
+    catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Adding Board", err });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id ) || !req.body) res.status(403).json({ message: "Invalid ID or Update", id: req.params.id, body: req.body });
+
+        const updated_board = await Boards.update( req.params.id, req.body );
+
+        res.status(200).json({ message: "Board Updated Successfully", updated_board });
+    }
+
+    catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Adding Board", err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        if( !utils.validateIdParam( req.params.id )) res.status(403).json({ message: "Invalid ID", id: req.params.id });
+
+        const result = await Boards.remove( req.params.id );
+    
+        res.status(200).json({ message: `Board:${req.params.id} removed`, result });
+    } 
+    
+    catch(err) {
+        console.log('----------' + err + '----------');
+
+        res.status(500).json({ message: "Error Removing Board", err });
+    }
+});
+
 module.exports = router;
