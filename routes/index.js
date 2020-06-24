e2c86e11f12562342251a73386f5ac6ca6e7c714
Trip4077 <bj2336@email.vccs.edu>
2020-06-23 21:01:18 -0400

    Format Update

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,8 @@ const course_routes = require( './model_routes/course_routes' );
 const unit_routes = require( './model_routes/unit_routes' );
 const material_routes = require( './model_routes/material_routes' );
 const certificate_routes = require( './model_routes/certificate_routes' );
-
 const admin_routes = require( './model_routes/admin_routes' );
 const instructor_routes = require( './model_routes/instructor_routes' );
-
 const message_routes = require( './model_routes/message_routes' );
 
 router.get('/', async (_, res) => res.json({ message: "Server Running", code: 200, status: "OK" }));
@@ -18,10 +16,8 @@ router.use('/courses', course_routes);
 router.use('/units', unit_routes);
 router.use('/materials', material_routes);
 router.use('/certificates', certificate_routes);
-
 router.use('/admins', admin_routes);
 router.use('/instructors', instructor_routes);
-
 router.use('/messages', message_routes);
 
 module.exports = router;
